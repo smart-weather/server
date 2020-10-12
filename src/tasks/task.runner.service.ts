e2368d1826bf670ce58213ae43e23298d4dac5a0
paddy <patrick@kuckelsberg.de>
2020-10-12 19:14:19 +0200

    [add] run a function once after x seconds.

--- src/tasks/task.runner.service.ts
+++ src/tasks/task.runner.service.ts
@@ -3,7 +3,22 @@ import { ITask } from "./task.interface";
 
 export class TaskRunner {
     constructor() {}
+    
+    public runTaskOnceAfterTimer(name: string, func: Function, timeToWait: number) {
+        let id: any = 0;
+        let task: ITask = {
+            func: func,
+            name: name,
+            interval: timeToWait
+        }
 
+        id = setInterval(() => {
+            task.func();
+
+            clearInterval(id);
+        }, task.interval);
+    }
+    
     public registerTask(name: string, func: Function, interval: number): ITask {
         let task: ITask = {
             name: name, 
